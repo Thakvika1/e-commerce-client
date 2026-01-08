f094b43ff60a6775727e9d00ea2d76105a813873
Tiki <thakvika.thorn@gmail.com>
2026-01-09 03:32:56 +0700

    refactor

--- src/data/navbarData.js
+++ src/data/navbarData.js
@@ -1,6 +1,10 @@
+import { HOME } from '../config/routes'
+import { LOGIN } from '../config/routes'
+import { REGISTER } from '../config/routes'
+
 export const navbarData = [
-  { name: 'Home', link: '/' },
-  { name: 'login', link: '/login' },
-  { name: 'register', link: '/register' },
+  { name: 'Home', link: HOME },
+  { name: 'login', link: LOGIN },
+  { name: 'register', link: REGISTER },
   { name: 'profile', link: '/user-detail' },
 ]
